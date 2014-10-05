2b88e880bbca74116f3ddfc0774ce2c90f525f8b
Steve Jarvis <steve.a.jarvis@gmail.com>
2014-10-05 00:01:03 -0400

    Trying to clean up some code, I wrote this a loooonnggg time ago.
    Just shuffling code around, deleting unneccessary lines, refactoring
    repeated code.

--- maketable.php
+++ maketable.php
@@ -1,65 +1,56 @@
 <?php
 include 'dbconnect.php';
 
-//Get vars from URL.
+// Get vars from URL.
 $comp = mysql_real_escape_string($_GET['comp']);
 $year = mysql_real_escape_string($_GET['year']);
 
-//Select all results for competition.
+// Select all results for competition.
 $query = mysql_real_escape_string("select * from $year$comp;");
 $result = mysql_query($query) or die ("Unable to fetch results.");
 	
-//Array of all the chars we want to not show up in the results.
-//Butch put extra odd record markers in there we're getting rid of.
-//Still mark records, but only with a simple * following.
+// Array of all the chars we want to not show up in the results.
+// Butch put extra odd record markers in there we're getting rid of.
+// Still mark records, but only with a simple * following.
 $needles = array("!","@","#","%","^","&","*");
 
 //Title..
 $readable = addSpace($comp);
-?><div id="title"><h2> <?php echo "<B>$year $readable</B><BR>";?></h2></div><?php
-	echo "<h3>WOMEN</h3>";
-//Now echo in a nice table.
-echo "<TABLE>";
-//Print one row for a header.
-echo "<SPAN id=\"header\"><TR><TD><h4>Class</h4></TD><TD><h4>Name</h4></TD><TD><h4>Birth</h4></TD><TD><h4>Team</h4></TD><TD><h4>Bodyweight</h4></TD>";
-echo "<TD><h4>Snatch 1</h4></TD><TD><h4>Snatch 2</h4></TD><TD><h4>Snatch 3</h4></TD><TD><h4>Jerk 1</h4></TD>";
-echo "<TD><h4>Jerk 2</h4></TD><TD><h4>Jerk 3</h4></TD><TD><h4>Total</h4></TD><TD><h4>Place</h4></TD></TR></SPAN>";
-//This loop prints each row.
-$ct=1;
+?>
+<div id="title"><h2> <?php echo "<B>$year $readable</B><BR>";?> </h2></div>
+<?php
+// Inserts a header for starting a fresh table.
+$ct = insertBeginTable();
+echo "<h3>WOMEN</h3>";
+insertGenericHeader();
+// This loop prints each row.
 $boys = false;
 for ($i=1; $i=mysql_fetch_row($result); $i++) {
     //Break for each class.
-    if($i[12] == '1' and $ct>1 and $i[0] != '56')
+    if($i[12] == '1' and $ct>1 and $i[0] != '56') 
         insertBlankRow();
-    //Big break for sex.
-    if(($i[0] == '56' and $boys == false) or ($i[0] == '62' and $boys == false)){
-        $ct = insertBoys();
+    // Big break for sex. It's unsure on which class we'll start, but (hopefully)
+    // it'll be one of either 56 or 62.
+    if(($i[0] == '56' or $i[0] == '62') and $boys == false){
+        $ct = insertBreakTable();
+        echo "<h3>MEN</h3>";
+        insertGenericHeader();
         $boys = true;
     }
+    // Classes can be used to set alternating colors for rows in css.
     if ($ct%2 == 0) echo ("<TR class=\"trtwo\">");
     else echo ("<TR class=\"trone\">");
     $ct++;
-    //Inside loop prints each column.
+    // Inside loop prints each column.
     for ($n=0; $n<13; $n++) {
         echo "<TD>";
         $colCT++; 
 			
-        //If we're between 5 an 10 check for weird chars.
-        //They will only occur as the 4th char in the string (e.g. 100&)
+        // If we're between columns 5 an 10 check for weird chars.
         if($n>4 and $n<11){
-            //Mark whether we've broken out of loop
-            $has_printed = false;
-            foreach($needles as $bad_char){
-                //As soon any weird string shows up we know they're there. Break.
-                if(strpos($i[$n], $bad_char) > 0){
-                    echo str_replace($needles, "", $i[$n])."<span style=\"color:red\">*</span>";
-                    $has_printed = true;
-                    break;
-                }
-            }
-            if(!$has_printed){
-                echo $i[$n];
-            }
+            // Replace anything in "needles" with nothing and follow the entry with a "*".
+            $startStr = str_replace($needles, "<span>*</span>", $i[$n]);
+            echo $startStr;
         }else{
             echo $i[$n];
         }
@@ -67,7 +58,7 @@ for ($i=1; $i=mysql_fetch_row($result); $i++) {
     }
     echo "</TR>";
 }
-echo "</TABLE>";
+insertEndTable();
 insertBlankRow();
 
 //Blank row between all classes
@@ -80,21 +71,34 @@ function insertBlankRow(){
 	echo "</TR>";
 }
 
-//Break into a new table for men
-function insertBoys(){
-	echo "</TABLE><BR>&nbsp<BR>";
-	echo "<h3>MEN</h3>";
+// Start of top table
+function insertBeginTable(){
 	echo "<TABLE width=100% >";
-	//Print one row for a header.
-	echo "<SPAN id=\"header\"><TR><TD><h4>Class</h4></TD><TD><h4>Name</h4></TD><TD><h4>Birth</h4></TD><TD><h4>Team</h4></TD><TD><h4>Bodyweight</h4></TD>";
-	echo "<TD><h4>Snatch 1</h4></TD><TD><h4>Snatch 2</h4></TD><TD><h4>Snatch 3</h4></TD><TD><h4>Jerk 1</h4></TD>";
-	echo "<TD><h4>Jerk 2</h4></TD><TD><h4>Jerk 3</h4></TD><TD><h4>Total</h4></TD><TD><h4>Place</h4></TD></TR></SPAN>";
-	
 	//Reset ct so table row colors sync
 	return 1;
 }
 
-//Change the comps to their name with a space
+//Break into a new table for men
+function insertBreakTable(){
+	echo "</TABLE><BR>&nbsp<BR>";
+	echo "<TABLE width=100% >";
+	// Reset ct so table row colors sync
+	return 1;
+}
+
+// Insert the generic header for table.
+function insertGenericHeader(){
+	// Print one row for a header.
+	echo "<SPAN id=\"header\"><TR><TD><h4>Class</h4></TD><TD><h4>Name</h4></TD><TD><h4>Birth</h4></TD><TD><h4>Team</h4></TD><TD><h4>Bodyweight</h4></TD>";
+	echo "<TD><h4>Snatch 1</h4></TD><TD><h4>Snatch 2</h4></TD><TD><h4>Snatch 3</h4></TD><TD><h4>Jerk 1</h4></TD>";
+	echo "<TD><h4>Jerk 2</h4></TD><TD><h4>Jerk 3</h4></TD><TD><h4>Total</h4></TD><TD><h4>Place</h4></TD></TR></SPAN>";
+}
+
+function insertEndTable() {
+    echo "</TABLE>";
+}
+
+// Change the comps to their name with a space
 function addSpace($competition){
 	if ($competition === 'NationalChampionship') return 'National Championship';
 	else if ($competition === 'AmericanOpen') return 'American Open';
@@ -102,11 +106,5 @@ function addSpace($competition){
 	else if ($competition === 'OlympicTrials') return 'Olympic Trials';
 	else if ($competition === 'SchoolageNationals') return 'Schoolage Nationals';
 	else if ($competition === 'CollegiateNationals') return 'Collegiate Nationals';
-}
-
-//Put each class in a div so we can click and go to it. Pass the class.
-function insertDiv($class){
-	echo "</DIV>";
-	echo "<DIV id=\"$class\">";
 }
 ?>
