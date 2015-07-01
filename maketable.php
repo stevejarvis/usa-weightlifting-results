59b5138d51bd3cf90bd46adf19fdb8a153bc9c3a
Steve Jarvis <steve.a.jarvis@gmail.com>
2015-06-30 21:26:21 -0400

    got things working with sqlite

--- maketable.php
+++ maketable.php
@@ -1,101 +1,112 @@
 <?php
-include 'dbconnect.php';
-include 'utils.php';
 
-// Get vars from URL.
-$comp = mysql_real_escape_string($_GET['comp']);
-$year = mysql_real_escape_string($_GET['year']);
+    header('Access-Control-Allow-Origin: http://www.nerdster.org');  
 
-// Select all results for competition.
-$query = mysql_real_escape_string("select * from $year$comp;");
-$result = mysql_query($query) or die ("Unable to fetch results.");
-	
-// Array of all the chars we want to not show up in the results.
-// Butch put extra odd record markers in there we're getting rid of.
-// Still mark records, but only with a simple * following.
-$needles = array("!","@","#","%","^","&","*","~","+");
+    include 'utils.php';
 
-//Title..
-$readable = prettyPrint($comp);
-?>
-<div id="title"><h2> <?php echo "<B>$year $readable</B><BR>";?> </h2></div>
-<?php
-// Inserts a header for starting a fresh table.
-$ct = insertBeginTable();
-echo "<h3>WOMEN</h3>";
-insertGenericHeader();
-// This loop prints each row.
-$boys = false;
-for ($i=1; $i=mysql_fetch_row($result); $i++) {
-    //Break for each class.
-    if($i[12] == '1' and $ct>1 and $i[0] != '56') 
-        insertBlankRow();
-    // Big break for sex. It's unsure on which class we'll start, but (hopefully)
-    // it'll be one of either 56 or 62.
-    if(($i[0] == '56' or $i[0] == '62') and $boys == false){
-        $ct = insertBreakTable();
-        echo "<h3>MEN</h3>";
-        insertGenericHeader();
-        $boys = true;
+    $dbLocation = 'usaw_results.sqlite';
+
+    $db = new SQLite3( $dbLocation );
+    if( !$db ) 
+    {
+        echo $db->lastErrorMsg();
     }
-    // Classes can be used to set alternating colors for rows in css.
-    if ($ct%2 == 0) echo ("<TR class=\"trtwo\">");
-    else echo ("<TR class=\"trone\">");
-    $ct++;
-    // Inside loop prints each column.
-    for ($n=0; $n<13; $n++) {
-        echo "<TD>";
-        $colCT++; 
-			
-        // If we're between columns 5 an 10 check for weird chars.
-        if($n>4 and $n<11){
-            // Replace anything in "needles" with nothing and follow the entry with a "*".
-            $startStr = str_replace($needles, "<span>*</span>", $i[$n]);
-            echo $startStr;
-        }else{
-            echo $i[$n];
+
+    // Get vars from URL.
+    $comp = $_GET['comp'];
+    $year = $_GET['year'];
+
+    // Select all results for competition.
+    $queryStr = SQLite3::escapeString( "SELECT * FROM \"$year$comp\";" );
+    $results = $db->query( $queryStr ) or die ( $db->lastErrorMsg() );
+        
+    // Array of all the chars we want to not show up in the results.
+    // Butch put extra odd record markers in there we're getting rid of.
+    // Still mark records, but only with a simple * following.
+    $needles = array("!","@","#","%","^","&","*","~","+");
+
+    //Title..
+    $readable = prettyPrint($comp);
+
+    echo "<div id=\"title\"><h2> <?php echo \"<B>$year $readable</B><BR> </h2></div>";
+
+    // Inserts a header for starting a fresh table.
+    $ct = insertBeginTable();
+    echo "<h3>WOMEN</h3>";
+    insertGenericHeader();
+
+    // This loop prints each row.
+    $boys = false;
+    while( $row = $results->fetchArray() ) {
+        //Break for each class.
+        if($row[12] == '1' and $ct>1 and $row[0] != '56') 
+            insertBlankRow();
+        // Big break for sex. It's unsure on which class we'll start, but (hopefully)
+        // it'll be one of either 56 or 62.
+        if(($row[0] == '56' or $row[0] == '62') and $boys == false){
+            $ct = insertBreakTable();
+            echo "<h3>MEN</h3>";
+            insertGenericHeader();
+            $boys = true;
         }
-        echo "</TD>";
+        // Classes can be used to set alternating colors for rows in css.
+        if ($ct%2 == 0) echo ("<TR class=\"trtwo\">");
+        else echo ("<TR class=\"trone\">");
+        $ct++;
+        // Inside loop prints each column.
+        for ($n=0; $n<13; $n++) {
+            echo "<TD>";
+            $colCT++; 
+                
+            // If we're between columns 5 an 10 check for weird chars.
+            if($n>4 and $n<11){
+                // Replace anything in "needles" with nothing and follow the entry with a "*".
+                $startStr = str_replace($needles, "<span>*</span>", $row[$n]);
+                echo $startStr;
+            }else{
+                echo $row[$n];
+            }
+            echo "</TD>";
+        }
+        echo "</TR>";
     }
-    echo "</TR>";
-}
-insertEndTable();
-insertBlankRow();
+    insertEndTable();
+    insertBlankRow();
 
-//Blank row between all classes
-function insertBlankRow(){
-	//Blank row here
-	echo "<TR>";
-	for($i=0; $i<13; $i++)
-        // Not sure I needed to do this...
-		echo "<TD>&nbsp</TD>";
-	echo "</TR>";
-}
+    //Blank row between all classes
+    function insertBlankRow(){
+        //Blank row here
+        echo "<TR>";
+        for($i=0; $i<13; $i++)
+            // Not sure I needed to do this...
+            echo "<TD>&nbsp</TD>";
+        echo "</TR>";
+    }
 
-// Start of top table
-function insertBeginTable(){
-	echo "<TABLE width=100% >";
-	//Reset ct so table row colors sync
-	return 1;
-}
+    // Start of top table
+    function insertBeginTable(){
+        echo "<TABLE width=100% >";
+        //Reset ct so table row colors sync
+        return 1;
+    }
 
-//Break into a new table for men
-function insertBreakTable(){
-	echo "</TABLE><BR>&nbsp<BR>";
-	echo "<TABLE width=100% >";
-	// Reset ct so table row colors sync
-	return 1;
-}
+    //Break into a new table for men
+    function insertBreakTable(){
+        echo "</TABLE><BR>&nbsp<BR>";
+        echo "<TABLE width=100% >";
+        // Reset ct so table row colors sync
+        return 1;
+    }
 
-// Insert the generic header for table.
-function insertGenericHeader(){
-	// Print one row for a header.
-	echo "<SPAN id=\"header\"><TR><TD><h4>Class</h4></TD><TD><h4>Name</h4></TD><TD><h4>Birth</h4></TD><TD><h4>Team</h4></TD><TD><h4>Bodyweight</h4></TD>";
-	echo "<TD><h4>Snatch 1</h4></TD><TD><h4>Snatch 2</h4></TD><TD><h4>Snatch 3</h4></TD><TD><h4>Jerk 1</h4></TD>";
-	echo "<TD><h4>Jerk 2</h4></TD><TD><h4>Jerk 3</h4></TD><TD><h4>Total</h4></TD><TD><h4>Place</h4></TD></TR></SPAN>";
-}
+    // Insert the generic header for table.
+    function insertGenericHeader(){
+        // Print one row for a header.
+        echo "<SPAN id=\"header\"><TR><TD><h4>Class</h4></TD><TD><h4>Name</h4></TD><TD><h4>Birth</h4></TD><TD><h4>Team</h4></TD><TD><h4>Bodyweight</h4></TD>";
+        echo "<TD><h4>Snatch 1</h4></TD><TD><h4>Snatch 2</h4></TD><TD><h4>Snatch 3</h4></TD><TD><h4>Jerk 1</h4></TD>";
+        echo "<TD><h4>Jerk 2</h4></TD><TD><h4>Jerk 3</h4></TD><TD><h4>Total</h4></TD><TD><h4>Place</h4></TD></TR></SPAN>";
+    }
 
-function insertEndTable() {
-    echo "</TABLE>";
-}
+    function insertEndTable() {
+        echo "</TABLE>";
+    }
 ?>
--- query.php
+++ query.php
@@ -1,62 +1,70 @@
 <?php
 
-include 'dbconnect.php';
-include 'utils.php';
+    header('Access-Control-Allow-Origin: http://www.nerdster.org');  
 
-//Get the competition we're looking for from the url.
-$key = $_GET['key'];
+    include 'utils.php';
 
-if(strlen($key) > 0){
-    // Sanitize
-	$key = mysql_real_escape_string($key);
+    //Get the competition we're looking for from the url.
+    $key = $_GET['key'];
 
-	// Select stuff. Cha ching. First look for athletes by the key.
-	$query =
-        "SELECT name, comp, year, class, title " .
-        "FROM athletes " .
-        "WHERE name LIKE '%$key%' " .
-        "ORDER BY year DESC;";
-	$resultAthletes = mysql_query($query) or die ("Unable to query database for athletes.");
+    $dbLocation = 'usaw_results.sqlite';
 
-    // Try to find competitions by the key too. Search table names directly.
-    $keynospace = preg_replace('/\s+/', '', $key);
-    $tablequery =
-        "SELECT TABLE_NAME " .
-        "FROM INFORMATION_SCHEMA.TABLES " .
-        "WHERE TABLE_NAME like '%$keynospace%' AND TABLE_TYPE = 'BASE TABLE' " .
-        "ORDER BY TABLE_NAME DESC;";
-    $resultComps = mysql_query($tablequery) or die ("Unable to get table names.");
-
-	// All the comps we have for the selected competition are in result[i]'s.
-	if ((mysql_num_rows($resultAthletes) == 0) &&
-        (mysql_num_rows($resultComps) == 0)){
-		echo "No results for " . $key . " as an athlete or competition.";
-	}
-    else {
-        // If there are results, get them and make a list. First athletes.
-        if( ! mysql_num_rows($resultAthletes) == 0){
-            echo "<h4>Athletes</h4>";
-            // Have to wrap everything in paragraph for style sheet.
-            echo "<p>";
-            while($row = mysql_fetch_array($resultAthletes)) {
-                echo "<a onclick='getTableFromSearch(\"$row[4]\",$row[2], \"$row[3]\"); return false;'> $row[0] as a $row[3] in the $row[2] $row[1].</a><BR>";
-            }
-            echo "</p>";
-        }
-        // Same for competitions
-        if (! mysql_num_rows($resultComps) == 0){
-            echo "<h4>Competitions</h4>";
-            // Have to wrap everything in paragraph for style sheet.
-            echo "<p>";
-            while($row = mysql_fetch_array($resultComps)) {
-                $compName = substr($row[0], 4);
-                $compYear = substr($row[0], 0, 4);
-                $prettyName = prettyPrint($compName);
-                echo "<a onclick='getTableFromSearch(\"$compName\",$compYear,\"tableHere\"); return false;'> The $compYear $prettyName.</a><BR>";
-            }
-            echo "</p>";
-        }
+    $db = new SQLite3( $dbLocation );
+    if( !$db ) 
+    {
+        echo $db->lastErrorMsg();
     }
-}
+
+    if( strlen($key) > 0 ){
+
+        // Select stuff. Cha ching. First look for athletes by the key.
+        $athleteQueryStr = SQLite3::escapeString( 
+            "SELECT name, comp, year, class, title " .
+            "FROM athletes " .
+            "WHERE name LIKE \"%$key%\" " .
+            "ORDER BY year DESC;" );
+        $resultAthletes = $db->query( $athleteQueryStr ) or die ( $db->lastErrorMsg() );
+
+        // Try to find competitions by the key too. Search table names directly.
+        $keynospace = preg_replace('/\s+/', '', $key);
+        $tableQueryStr = SQLite3::escapeString( 
+            "SELECT name " .
+            "FROM sqlite_master " .
+            "WHERE type=\"table\" " .
+            "AND name LIKE \"%$keynospace%\" " .
+            "ORDER BY name DESC;" );
+        $resultComps = $db->query( $tableQueryStr ) or die ( $db->lastErrorMsg() );
+
+        // If there are results, get them and make a list. First athletes.
+        $athletesFound = false;
+        echo "<h4>Athletes</h4>";
+        // Have to wrap everything in paragraph for style sheet.
+        echo "<p>";
+        while( $row = $resultAthletes->fetchArray() ) {
+            echo "<a onclick='getTableFromSearch(\"$row[4]\",$row[2], \"$row[3]\"); return false;'> $row[0] as a $row[3] in the $row[2] $row[1].</a><BR>";
+            $athletesFound = true;
+        }
+        if( ! $athletesFound )
+            echo "No athletes found like '" . $key . "'.";
+
+        echo "</p>";
+        
+        // Same for competitions
+        $compsFound = false;
+        echo "<h4>Competitions</h4>";
+        // Have to wrap everything in paragraph for style sheet.
+        echo "<p>";
+        while( $row = $resultComps->fetchArray() ) {
+            $compName = substr($row[0], 4);
+            $compYear = substr($row[0], 0, 4);
+            $prettyName = prettyPrint($compName);
+            echo "<a onclick='getTableFromSearch(\"$compName\",$compYear,\"tableHere\"); return false;'> The $compYear $prettyName.</a><BR>";
+            $compsFound = true;
+        }
+        if( ! $compsFound )
+            echo "No competitions found like '" . $key . "'.";
+
+        echo "</p>";
+    }
 
 ?>
